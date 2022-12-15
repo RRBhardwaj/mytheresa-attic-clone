edcf8f339aaff6472d66761743d771138e30dec3
Rishi <rishirajbhardwaj20@gmail.com>
2022-12-15 23:17:19 +0530

    New Commit

--- mytheresa-attic/src/Pages/Women.jsx
+++ mytheresa-attic/src/Pages/Women.jsx
@@ -15,30 +15,51 @@ const Women = () => {
         <Spacer/>
         <Text paddingTop="30px" paddingBottom="8px" fontSize='lg'>DON'T MISS OUT</Text>
         <Text fontSize='3xl'>NEW ARRIVALS</Text>
-        <Button bg="black" color="white" variant='outline' marginBottom="20px" marginTop="10px">SHOP NOW >></Button>
+        <Button bg="black" color="white" variant='outline' marginBottom="20px" marginTop="10px" _hover="black">SHOP NOW >></Button>
       </Box>
       <Grid
       className={styles.topfourthsection}
-        h='1200px'
+        h='1180px'
         templateRows='repeat(4, 1fr)'
         templateColumns='repeat(4, 1fr)'
         gap={4}
       >
-        <GridItem rowSpan={4} colSpan={2} bg='#f2f7f7'>
+        <GridItem rowSpan={4} colSpan={2} bg='white'>
           <Text marginTop="20px" fontSize='sm'>PARIS TEXAS X MYTHERESA</Text>
           <Text fontSize='3xl'>The Exclusive Capsule</Text>
           <Text fontSize='3xl'>Collection</Text>
           <Image marginLeft="5px" marginTop="10px" src="https://img.mytheresa.com/media/static/raw/cms/l/LIFE_HP_2022_CW50/Paris-Texas-ActboxTall_2x_20221212102147.jpg?imwidth=600&imdensity=1" alt="picture"/>
-          <Button bg="black" color="white" variant='solid' marginBottom="20px" marginTop="20px">SHOP NOW >></Button>
+          <Button size='md' bg="black" color="white" variant='solid' marginBottom="20px" marginTop="20px" _hover="black">SHOP NOW >></Button>
         </GridItem>
-        <GridItem rowSpan={2} colSpan={2} bg='papayawhip'>
-        <Text marginTop="20px" fontSize='sm'>SEASONAL FLATS</Text>
-          <Text fontSize='3xl'>The Exclusive Capsule</Text>
-          <Text fontSize='3xl'>Collection</Text>
-          <Image marginLeft="5px" marginTop="10px" src="" alt="picture"/>
-          <Button bg="black" color="white" variant='solid' marginBottom="20px" marginTop="20px">SHOP NOW >></Button>
+        <GridItem rowSpan={2} colSpan={2} bg='white'>
+          <Text marginTop="20px" fontSize='sm'>SEASONAL FLATS</Text>
+          <Text fontSize='3xl'>Because cozy feet are</Text>
+          <Text fontSize='3xl'>happy feet</Text>
+          <Image marginLeft="5px" marginTop="12px" src="https://img.mytheresa.com/media/static/raw/cms/l/WW_HP_2022_CW50/CW50_WW_HP_ActboxSmall_3_2x_20221212110651.jpg?imwidth=600&imdensity=1" alt="picture"/>
+          <Button bg="black" color="white" variant='solid'_hover="black" marginTop="25px" marginBottom="50px" size="md">SHOP NOW >></Button>
+          <hr/>
         </GridItem>
-        <GridItem rowSpan={2} colSpan={2} bg='tomato'></GridItem>
+        <GridItem rowSpan={2} colSpan={2} bg='white'>
+          <Text marginTop="20px" fontSize='sm'>CONTEMPORARY JEWELRY</Text>
+          <Text fontSize='3xl'>Craft and style rolled into</Text>
+          <Text fontSize='3xl'>one</Text>
+          <Image marginLeft="5px" marginTop="18px" src="https://img.mytheresa.com/media/static/raw/cms/l/WW_HP_2022_CW50/CW50_WW_HP_ActboxSmall_4_2x_20221212110650.jpg?imwidth=600&imdensity=1" alt="picture"/>
+          <Button bg="black" color="white" variant='solid'_hover="black" marginTop="30px" marginBottom="50px" size="md">SHOP NOW >></Button>
+        </GridItem>
+      </Grid>
+      <Box marginTop="20px" marginBottom="20px" width="80%" marginLeft="150px">
+        <hr/>
+      </Box>
+      <Grid
+      className={styles.topfourthsection}
+        h='1180px'
+        templateRows='repeat(4, 1fr)'
+        templateColumns='repeat(4, 1fr)'
+        gap={4}
+      >
+        <GridItem rowSpan={2} colSpan={2} bg='tomato' />
+        <GridItem rowSpan={4} colSpan={2} bg='papayawhip' />
+        <GridItem rowSpan={2} colSpan={2} bg='tomato' />
       </Grid>
     </Box>
   )
